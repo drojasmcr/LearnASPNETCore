56cd1aa06479e8dbd10c9a9367de44610984c5b0
unknown <diego@rojascr.net>
2020-06-27 13:08:58 -0600

    Adding component for statistics

--- Globomantics/Startup.cs
+++ Globomantics/Startup.cs
@@ -24,6 +24,7 @@ namespace Globomantics
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddSignalR();
             services.AddControllersWithViews();
 
             services.AddSingleton<IConferenceService, ConferenceMemoryService>();
